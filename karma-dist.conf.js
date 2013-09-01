02c3c2329c0d3930929b2358adbd61af0a51179f
Rohan Jain <crodjer@gmail.com>
2013-09-01 14:43:28 +0530

    Update development dependencies
    Significant change: New module based config format
    
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- karma-dist.conf.js
+++ karma-dist.conf.js
@@ -1,53 +1,67 @@
-// Karma E2E configuration
+// Karma configuration
+// Generated on Sun Sep 01 2013 14:32:36 GMT+0530 (IST)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'src/deps/utils.js',
-  'dist/saber.js',
-  'test/**/*.js'
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [];
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['dots'];
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-// web server port
-port = 8080;
 
-// cli runner port
-runnerPort = 9100;
+    // list of files / patterns to load in the browser
+    files: [
+      'src/deps/utils.js',
+      'dist/saber.js',
+      'test/**/*.js'
+    ],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // list of files to exclude
+    exclude: [],
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // web server port
+    port: 9877,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+  });
+};
--- karma.conf.js
+++ karma.conf.js
@@ -1,63 +1,80 @@
 // Karma configuration
+// Generated on Sun Sep 01 2013 14:32:36 GMT+0530 (IST)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'src/*.js',
-  'src/**/*.js',
-  'test/unit/*.js',
-  'test/unit/**/*.js'
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-preprocessors = {
-  'src/**/*.js': 'coverage'
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'src/*.js',
+      'src/**/*.js',
+      'test/unit/*.js',
+      'test/unit/**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      'src/intro.js',
+      'src/outro.js'
+    ],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress', 'coverage'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+
+    preprocessors: {
+      'src/**/*.js': 'coverage'
+    },
+
+    coverageReporter: {
+      type : 'lcov',
+      dir : '.coverage/',
+    }
+  });
 };
-
-// list of files to exclude
-exclude = ['src/intro.js', 'src/outro.js'];
-
-coverageReporter = {
-  type : 'lcov',
-  dir : '.coverage/',
-}
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['dots', 'coverage'];
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
